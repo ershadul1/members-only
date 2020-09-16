da3a526fc55adb36a361b82bd3979b10bd1a81f4
ershadul1 <ershadul.rayhan@gmail.com>
2020-09-16 15:32:11 +0600

    Add user_status column in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_141230) do
+ActiveRecord::Schema.define(version: 2020_09_16_093029) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_09_15_141230) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_status", default: "idle"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
